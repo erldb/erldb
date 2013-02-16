061a0bd2759b5b86de73545b60b7763be5ef100d
Niclas Axelsson <niclas@burbas.se>
2013-02-16 16:41:20 +0100

    More functionality to erl_db

--- src/erl_db.erl
+++ src/erl_db.erl
@@ -6,3 +6,22 @@ squery(PoolName, Sql) ->
     poolboy:transaction(PoolName, fun(Worker) ->
         gen_server:call(Worker, {squery, Sql})
     end).
+
+
+find(Model, Id) ->
+    Poolname = Model:backend(),
+    poolboy:transaction(Poolname, fun(Worker) ->
+                                          gen_server:call(Worker, {find, Model, Id})
+                                  end).
+
+delete(Model) when is_tuple(Model) ->
+    Poolname = Model:backend(),
+    poolboy:transaction(Poolname, fun(Worker) ->
+                                          gen_server:call(Worker, {delete, Model})
+                                  end).
+
+save(Model) when is_tuple(Model) ->
+    Poolname = Model:backend(),
+    poolboy:transaction(Poolname, fun(Worker) ->
+                                          gen_server:call(Worker, {save, Model})
+                                  end).
