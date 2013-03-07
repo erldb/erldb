71688878fcf9d46040496d2f127fbd4e5570a1a8
Niclas Axelsson <niclas@burbas.se>
2013-03-07 21:48:42 +0100

    Use regular io:format in logging. It's better in this early state :)

--- src/erl_db_log.erl
+++ src/erl_db_log.erl
@@ -2,7 +2,9 @@
 
 -export([msg/3]).
 
--define(LOG(Level, Message, Params), lager:log(Level, self(), Message, Params)).
+%-define(LOG(Level, Message, Params), lager:log(Level, self(), Message, Params)).
+
+-define(LOG(Level, Message, Params), io:format("################## ~p ###################~n", [Level]), io:format(Message ++ "~n", Params)).
 
 msg(Level, Message, Params) ->
     ?LOG(Level, Message, Params).
