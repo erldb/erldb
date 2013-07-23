6d85dda3ef1db85e0cbbf1049c381824f0e4669c
Niclas Axelsson <niclas@burbas.se>
2013-07-23 14:55:33 +0200

    Remove old debug-message

--- src/erl_db_sup.erl
+++ src/erl_db_sup.erl
@@ -25,7 +25,6 @@ start_link() ->
 
 init([]) ->
     Pools = erl_db_env:get_env(erl_db, db_pools, []),
-    io:format("Pools: ~p~n", [Pools]),
     PoolSpecs = lists:map(fun({Name, Type, SizeArgs, WorkerArgs}) ->
                                   PoolArgs = [{name, {local, Name}},
                                               {worker_module, Type},
