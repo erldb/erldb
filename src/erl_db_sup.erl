8cdf9fde04737ed60789363869b4f6fcc631fe19
Niclas Axelsson <niclas@burbas.se>
2013-07-23 14:27:16 +0200

    Make erl_db compliant with the version of poolboy that ChicagoBoss uses

--- src/erl_db_sup.erl
+++ src/erl_db_sup.erl
@@ -24,10 +24,13 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, Pools} = erl_db_env:get_env(erl_db, db_pools),
+    Pools = erl_db_env:get_env(erl_db, db_pools, []),
+    io:format("Pools: ~p~n", [Pools]),
     PoolSpecs = lists:map(fun({Name, Type, SizeArgs, WorkerArgs}) ->
-        PoolArgs = [{name, {local, Name}},
-                    {worker_module, Type}] ++ SizeArgs,
-        poolboy:child_spec(Name, PoolArgs, WorkerArgs)
-    end, Pools),
+                                  PoolArgs = [{name, {local, Name}},
+                                              {worker_module, Type},
+                                              {worker_args, WorkerArgs}
+                                             ] ++ SizeArgs,
+                                  {Name, {poolboy, start_link, [PoolArgs]}, permanent, 5000, worker, [poolboy]}
+                          end, Pools),
     {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
