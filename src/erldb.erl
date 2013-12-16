71dcc6d843c10753f499f3241b53f96d2de8d82a
Niclas Axelsson <niclas@burbas.se>
2013-12-16 21:17:40 +0100

    Reworked the whole module to work with the new model-specification

--- src/erldb.erl
+++ src/erldb.erl
@@ -1,18 +1,18 @@
-%%% @author Niclas Axelsson <niclas@burbas.se>
+%%%-------------------------------------------------------------------
+%%% @author Niclas Axelsson <niclas@burbas.se> [http://www.burbas.se]
+%%% @copyright (C) 2013, Niclas Axelsson
 %%% @doc
-%%% The main interface for erldb
+%%% Main interface for erldb
 %%% @end
-%%% Created : 26 Jul 2013 by Niclas Axelsson <niclas@burbas.se>
+%%% Created : 16 Dec 2013 by Niclas Axelsson
+%%%-------------------------------------------------------------------
 -module(erldb).
 -export([
          start/1,
          stop/0,
          find/2,
          delete/1,
-         delete/2,
          save/1,
-         read/2,
-         get_models/1
         ]).
 
 start(_Args) ->
@@ -32,14 +32,13 @@ find(Model, Conditions) ->
 
 find(Model, Conditions, Options) ->
     Attributes = Model:module_info(attributes),
-    Poolnames = proplists:get_value(backend, Attributes, []),
+    [Poolnames|_] = proplists:get_value(backend, Attributes, []),
     lists:foldl(
       fun({Poolname, _Args}, Results) ->
               Worker = poolboy:checkout(Poolname),
-              NormalizedConditions = normalize_conditions(Conditions),
-              case is_conditions_supported(Worker, NormalizedConditions) of
+              case gen_server:call(Worker, {supported_condition, Conditions}) of
                   ok ->
-                      {ok, Res} = gen_server:call(Worker, {find, Model, normalize_conditions(Conditions), Options}),
+                      {ok, Res} = gen_server:call(Worker, {find, Model, Conditions, Options}),
                       poolboy:checkin(Poolname, Worker),
                       Res;
                   {not_supported, Operator} ->
@@ -56,119 +55,146 @@ find(Model, Conditions, Options) ->
 %% @end
 %%--------------------------------------------------------------------
 delete(Object) when is_tuple(Object) ->
-    Model = element(1, Object),
-    Fields = proplists:get_value(fields, Model:module_info(attributes), []),
-    [_|Values] = erlang:tuple_to_list(Object),
-    Conditions = lists:zipwith(fun({Fieldname, _, _}, Value) ->
-                                       {Fieldname, Value}
-                               end, Fields, Values),
-    delete(Model, Conditions).
+    Module = element(1, Object),
+    Proceed =
+        case erlang:function_exported(Module, '_pre_delete', 1) of
+            true ->
+                case Module:'_pre_delete'(Object) of
+                    stop ->
+                        stop;
+                    _ ->
+                        ok
+                end;
+            _ ->
+                ok
+        end,
 
-%%--------------------------------------------------------------------
-%% @doc Deletes objects of type 'Model' with the given 'conditions'
-%% @spec delete(Model :: atom(), Conditions :: [{atom(), any()}]) -> {ok, DeletedObjects :: integer()}
-%% @end
-%%--------------------------------------------------------------------
-delete(Model, Conditions) when is_atom(Model) ->
-    Poolnames = proplists:get_value(backend, Model:module_info(attributes), []),
-    lists:foreach(
-      fun({Poolname, _Args}) ->
-              Worker = poolboy:checkout(Poolname),
-              NormalizedConditions = normalize_conditions(Conditions),
-              case is_conditions_supported(Worker, NormalizedConditions) of
-                  ok ->
-                      gen_server:call(Worker, {delete, Model, NormalizedConditions});
-                  {not_supported, Operator} ->
-                      erldb_log:msg(error, "Backend beloning to '~p' does not support query operator '~p'", [Model, Operator])
-              end,
-              poolboy:checkin(Poolname, Worker)
-      end, Poolnames).
+    case Proceed of
+        ok ->
+            lists:foldl(
+              fun({Poolname, Arguments}, Results) ->
+                      Worker = poolboy:checkout(Poolname),
+                      Res = gen_server:call(Worker, {delete, Object, Arguments}),
+                      poolboy:checkin(Poolname, Worker),
+                      Res
+              end, proplists:get_value(backend, Module:module_info(attributes, [])));
+        _ ->
+            ok
+    end,
 
-
-%%--------------------------------------------------------------------
-%% @doc Saves the specified 'Object' to the database.
-%% @spec save(Object :: tuple()) -> ok | {error, Reason}
-%% @end
-%%--------------------------------------------------------------------
-save(Object) when is_tuple(Object) ->
-    Modelname = element(1, Object),
-    Attributes = Modelname:module_info(attributes),
-    [{Poolname, _PoolArgs}] = proplists:get_value(backend, Attributes),
-    Worker = poolboy:checkout(Poolname),
-    Res = gen_server:call(Worker, {save, Object}),
-    poolboy:checkin(Poolname, Worker),
-    Res.
-
-%%--------------------------------------------------------------------
-%% @doc Saves the specified 'Object' to the database.
-%% @spec save(Object :: tuple()) -> ok | {error, Reason}
-%% @end
-%%--------------------------------------------------------------------
-read(Modelname, Key) ->
-    Attributes = Modelname:module_info(attributes),
-    [{Poolname, _PoolArgs}] = proplists:get_value(backend, Attributes),
-    Worker = poolboy:checkout(Poolname),
-    Res = gen_server:call(Worker, {read, Modelname, Key}),
-    poolboy:checkin(Poolname, Worker),
-    Res.
-%%--------------------------------------------------------------------
-%% @doc Gets all models that's beloning to a specific, named, backend
-%% @spec get_models(Backend :: atom()) -> [ Modelname :: atom() ].
-%% @end
-%%--------------------------------------------------------------------
-get_models(Backend) when is_atom(Backend) ->
-    LoadedModules = [ X || {X, _} <- code:all_loaded() ],
-    lists:filter(
-      fun(Modulename) ->
-              case proplists:get_value(backend, Modulename:module_info(attributes)) of
-                  undefined ->
-                      false;
-                  Backendlist ->
-                      lists:any(
-                        fun({ModelBackend, _}) when Backend == ModelBackend ->
-                                true;
-                           (_) ->
-                                false
-                        end, Backendlist)
-              end
-      end, LoadedModules).
-
-%%---------------------------------------
-%% INTERNAL FUNCTIONS
-%%---------------------------------------
-
-%%--------------------------------------------------------------------
-%% @doc Checks if all 'Conditions' are supported at the targeting 'Worker'
-%% @spec is_conditions_supported(Worker :: pid(), Conditions :: list() -> ok | {not_supported, Operator :: atom()}
-%% @end
-%%--------------------------------------------------------------------
-is_conditions_supported(Worker, Conditions) when is_pid(Worker) ->
-    {ok, SupportedConditions} = gen_server:call(Worker, {supported_conditions}),
-    is_conditions_supported_aux(Conditions, SupportedConditions).
-
-is_conditions_supported_aux([], _) -> ok;
-is_conditions_supported_aux([{_, Operator, _}|Tl], SupportedConditions) ->
-    case lists:member(Operator, SupportedConditions) of
-        false ->
-            {not_supported, Operator};
-        true ->
-            is_conditions_supported_aux(Tl, SupportedConditions)
+    case {Proceed, erlang:function_exported(Module, '_post_delete', 1)} of
+        {stop, _} ->
+            ok;
+        {_, true} ->
+            case Module:'_post_delete'(Object) of
+                rollback ->
+                    Object:save(); %% Save the object back to database
+                _ ->
+                    ok
+            end;
+        _ ->
+            ok
     end.
 
-normalize_conditions(Conditions) ->
-    normalize_conditions(Conditions, []).
+%%--------------------------------------------------------------------
+%% @doc Saves the specified 'Object' to the database.
+%% @spec save(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec save(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+save(Object) when is_tuple(Object) ->
+    Module = element(1, Object),
+    %% Determine if this is an insert or an update
+    [PrimaryKey] =
+        lists:filter(
+          fun({field, [{_, _, _, Arglist}]}) -> proplists:get_value(primary_key, Arglist, false); (_) -> false end,
+          Module:module_info(attributes)),
+    PrimaryKeyPos = element(2, PrimaryKey),
+    case element(PrimaryKeyPos, Object) of
+        'id' ->
+            %% This is an insertion
+            insert(Object);
+        _ ->
+            update(Object)
+    end.
 
-normalize_conditions([], Acc) ->
-    lists:reverse(Acc);
-normalize_conditions([Key, Operator, Value|Rest], Acc) when is_atom(Key), is_atom(Operator) ->
-    normalize_conditions(Rest, [{Key, Operator, Value}|Acc]);
-normalize_conditions([{Key, Value}|Rest], Acc) when is_atom(Key) ->
-    normalize_conditions(Rest, [{Key, 'equals', Value}|Acc]);
-normalize_conditions([{Key, 'eq', Value}|Rest], Acc) when is_atom(Key) ->
-    normalize_conditions(Rest, [{Key, 'equals', Value}|Acc]);
-normalize_conditions([{Key, 'ne', Value}|Rest], Acc) when is_atom(Key) ->
-    normalize_conditions(Rest, [{Key, 'not_equals', Value}|Acc]);
-normalize_conditions([{Key, Operator, Value}|Rest], Acc) when is_atom(Key), is_atom(Operator) ->
-    normalize_conditions(Rest, [{Key, Operator, Value}|Acc]);
-normalize_conditions([{Key, Operator, Value, Options}|Rest], Acc) when is_atom(Key), is_atom(Operator), is_list(Options) ->
-    normalize_conditions(Rest, [{Key, Operator, Value, Options}|Acc]).
+%%--------------------------------------------------------------------
+%% @doc Performs an update operation for an object
+%% @spec update(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec update(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+update(Object) when is_tuple(Object) ->
+    Module = elemnent(1, Object),
+    {Proceed, NewObject} =
+        case erlang:function_exported(Module, '_pre_update', 1) of
+            true ->
+                case Module:'_pre_update'(Object) of
+                    stop ->
+                        {stop, undefined};
+                    {ok, Obj} ->
+                        {ok, Obj}
+                end;
+            _ ->
+                {ok, Object}
+        end,
+
+    UpdateRes =
+        case Proceed of
+            {ok, _} ->
+                Res =
+                    lists:foldl(
+                      fun({Poolname, Arguments}, Results) ->
+                              Worker = poolboy:checkout(Poolname),
+                              Res = gen_server:call(Worker, {update, NewObject, Arguments}),
+                              poolboy:checkin(Poolname, Worker),
+                              Res
+                      end, proplists:get_value(backend, Module:module_info(attributes, []))),
+                lists:any(fun({ok, _}) -> true; (_) -> false end, Res),
+            _ ->
+                []
+        end,
+
+    case {Proceed, erlang:function_exported(Module, '_post_update', 1)} of
+        {stop, _} ->
+            {stopped, Object};
+        {ok, true} ->
+            Module:'_post_update'(NewObject, UpdateRes);
+        {_, {error, Reason}} ->
+            {error, Reason};
+        _ ->
+            {ok, New}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Performs an insert operation for an object
+%% @spec insert(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+%% @end
+%%--------------------------------------------------------------------
+-spec insert(tuple()) -> {ok, tuple()} | {stopped, tuple()} | {error, atom()}.
+insert(Object) when is_tuple(Object) ->
+    Module = element(1, Object),
+    {Proceed, NewObject} =
+        case erlang:function_exported(Module, '_pre_insert', 1) of
+            true ->
+                case Module:'_pre_insert'(Object) of
+                    stop ->
+                        {stop, undefined};
+                    _ ->
+                        %% @TODO Update the object. Should return in format {ok, Object} or {error, Reason}
+                        ok
+                end;
+            _ ->
+                %% @TODO Update the object. Should return in format {ok, Object} or {error, Reason}
+        end,
+
+    case {Proceed, erlang:function_exported(Module, '_post_insert', 1)) of
+        {stop, _} ->
+            {stopped, Object};
+        {ok, true} ->
+            Module:'_post_insert'(NewObject);
+        {_, {error, Reason}} ->
+            {error, Reason};
+        _ ->
+            {ok, NewObject}
+    end.
