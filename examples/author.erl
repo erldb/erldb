dc085e5134acecb7f05d865e14cd6c516ed4c3bd
Niclas Axelsson <niclas@burbas.se>
2014-08-15 15:37:20 +0200

    Remove -module attribute since we don't use it.

--- examples/author.erl
+++ examples/author.erl
@@ -3,13 +3,8 @@
 %%% Example of a author-model used in erldb
 %%% @end
 %%% Created : 15 Aug 2014 by Niclas Axelsson <niclas@burbas.se>
--module(author).
-
-
-
-
 -backend(mnesia, []).
 -relation(has, many, tags).
 
 -field(id, integer, [primary_key, auto_increment]).
--field(name, string).
+-field(name, string, [{default, "John Doe"}]).
