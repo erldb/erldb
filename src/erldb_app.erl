f9dc8c51b909a0e1d2752b265c720c336816e0c6
Niclas Axelsson <niclas@burbas.se>
2014-09-12 12:16:46 +0200

    *** empty log message ***

--- src/erldb_app.erl
+++ src/erldb_app.erl
@@ -25,3 +25,37 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+bootstrap_models([]) ->
+    ok;
+bootstrap_models([H|T]) ->
+    %% We always compile the models we load.
+    {ok, Model, Binary) = erldb_compiler:compile(H),
+    code:load_binary(Model, H, Binary),
+    Attr = Model:module_info(attributes),
+
+    %% We should be able to handle default backends
+    Backend =
+        case proplists:get_value(backend, Attr) of
+            undefined ->
+                {ok, B} = application:get_env(erldb, default_backend),
+                B;
+            [{B, _}] ->
+                B
+        end,
+
+    %% Take out the db_pools information
+    {ok, Env} = application:get_env(erldb, db_pools),
+    BackendValues = proplists:get_value(Backend, Env),
+
+    TableOptions = proplists:get_value(default_table_options, BackendValues),
+    Worker = poolboy:checkout(Backend),
+
+    {ok, Model} = gen_server:call(Worker, {init_table, Model, [{module_attr, Attr} | TableOptions]}),
+    poolboy:checkin(Backend, Worker),
+    ensure_tables(T).
